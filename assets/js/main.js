dab6daf899c6bbef1e0a7e3d8035d02f0a1d2853
Jonas Holanda <jonasholanda15@gmail.com>
2024-05-05 23:10:01 -0300

    feat: desafio1 concluído

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+const state = {
+    modal: new bootstrap.Modal(document.querySelector('#editar')),
+    form: document.querySelector('#form-editar'),
+    cardSection: {
+        nomeUser: document.querySelector('#nome-user'),
+        idadeUser: document.querySelector('#idade-user'),
+        cepUser: document.querySelector('#cep-user'),
+        ruaUser: document.querySelector('#rua-user'),
+        bairroUser: document.querySelector('#bairro-user'),
+        cidadeUser: document.querySelector('#cidade-user'),
+        estadoUser: document.querySelector('#estado-user'),
+        imagemUser: document.querySelector('#imagem-user'),
+        numCasaUser: document.querySelector('#num_casa-user'),
+        bioUser: document.querySelector('#bio-user')
+    },
+    formValues: {
+        nomeUser: document.querySelector('input[name="nome_user-form"]'),
+        idadeUser: document.querySelector('input[name="idade_user-form"]'),
+        cepUser: document.querySelector('input[name="cep_user-form"]'),
+        ruaUser: document.querySelector('input[name="rua_user-form"]'),
+        bairroUser: document.querySelector('input[name="bairro_user-form"]'),
+        cidadeUser: document.querySelector('input[name="cidade_user-form"]'),
+        estadoUser: document.querySelector('input[name="estado_user-form"]'),
+        imagemUser: document.querySelector('input[name="imagem_user-form"]'),
+        numCasaUser: document.querySelector('input[name="num_user-form"]'),
+        bioUser: document.querySelector('textarea[name="bio_user-form"]')
+    }
+}
+
+state.form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let qtdIguais = 0;
+
+    for (const i in state.cardSection) {
+        let valorCard = i == "imagemUser" ? state.cardSection[i].src : state.cardSection[i].innerText;
+        let valorForm = state.formValues[i];
+
+        if (valorCard === valorForm.value) qtdIguais++;
+    }
+    
+    if (qtdIguais === 10) {
+        state.modal.hide();
+        Swal.fire({
+            title: "Sem Alterações!",
+            text: "Não houve alteração nos dados!",
+            icon: "info"
+          });
+
+    } else {
+
+        let camposVazios = false;
+        for (const i in state.formValues) {
+            const valorForm = state.formValues[i];
+            const nomeCampo = valorForm.placeholder;
+            const divPai = valorForm.parentElement;
+            
+            const alerta = divPai.nextElementSibling;
+            
+            if (valorForm.value === "") {
+                if (alerta) {
+                    alerta.remove();
+                }    
+
+                divPai.insertAdjacentHTML("afterend", `<div class="text-danger mb-3">Campo ${nomeCampo} é obrigatório!</div>`);
+             
+                camposVazios = true;
+            }
+        }
+       
+        if (!camposVazios) {
+
+            for (const i in state.formValues) {
+                let valorForm = state.formValues[i];
+                i == "imagemUser" ? state.cardSection[i].src = valorForm.value : state.cardSection[i].innerText = valorForm.value; 
+            }
+
+            state.modal.hide();
+
+            Swal.fire({
+                title: "Sucesso!",
+                text: "Dados alterados com sucesso!",
+                icon: "success"
+            });
+        }
+    }
+
+});
+
+document.querySelector('#chamaModal').addEventListener('click', () => {
+    for (const i in state.formValues) {
+        let valorForm = state.formValues[i];
+        i == "imagemUser" ? valorForm.value = state.cardSection[i].src : valorForm.value = state.cardSection[i].innerText;  
+        
+        const divPai = valorForm.parentElement;
+        const alerta = divPai.nextElementSibling;
+        if (alerta) {
+            alerta.remove();
+        } 
+        
+    }
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+<div align="center">
+
+# Desafio 1 - Sync360.io
+
+**Desafio para o processo seletivo da Sync360.io**
+</div>
+
+- **Utilizei os recursos do bootstrap para ganhar tempo na criação desta página.**
+
+- **Optei por fazer o formulário em uma janela modal, assim o usuário precisaria clicar em um botão para poder fazer as alterações necessárias nas suas informações.**
+
+- **Utilizei as bibliotecas de ícones(Iconify) e de pop-up(SweetAlert2) para deixar o site elegante.**
+
+- **No meu formulário adicionei a validação de campo obrigatório, e duas mensagens personalizadas. Uma de sucesso, quando ocorre uma alteração. E uma informando que não há alterações, caso não haja nenhuma mudança nos valores dos campos.**
+
+- **Coloquei os campos do formulário, bem como os valores da seção do usuário dentro de objetos, para facilitar na hora das validações e atribuições de valores. Pois como eles tem a mesma quantidade de campos, utilizei um laço de repetição. Ao invés de ficar atribuindo valores manualmente.**
+
+# Tecnologias Utilizadas
+<div align="center">
+
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![css](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![javascript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E) ![Bootstrap](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)
+
+</div>
+
+**Link do site: [https://jonas-holanda.github.io/desafio1-sync360.io](https://jonas-holanda.github.io/desafio1-sync360.io)**
